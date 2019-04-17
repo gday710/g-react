074b16d95906ccf77a002b1fdb767cb110e91f8a
gday710 <gracenday@gmail.com>
2019-04-17 14:10:41 -0400

    fetched pizza data from rails app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {pizza:[]};
+  }
+
+  async getPizza() {
+    const response = await fetch('http://localhost:3001');
+    const pizza = await response.json();
+    this.setState({pizza: pizza});
+  }
+
+  componentDidMount() {
+    this.getPizza();
+  }
+
   sayHello(name) {
-    return `Hello ${name}`
+    return `Hello ${name}`;
     //return "string literal";
   }
 
   render() {
+    const pizza = this.state.pizza;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          Hi Adam! It's G. :)
+          {pizza.join(', ')}
           </p>
           <a
             className="App-link"
